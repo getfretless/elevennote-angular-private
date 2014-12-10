03be06d48278840d1594b2cb1e8bcafb9cc1d392
David Jones <unixmonkey1@gmail.com>
2014-12-10 13:06:58 -0500

    Create note form

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -3,18 +3,22 @@
 var elevennoteBasePath = 'http://localhost:3000/api/v1/';
 var apiKey = '$2a$10$5GO4Gw5d9Snpy.KfhHDhJesJ4XKZ7iCTJaeUO0D0Z51T586TEbUTa';
 
-angular.module('myApp.notes', ['ngRoute'])
+var noteApp = angular.module('myApp.notes', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+noteApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/index.html',
     controller: 'NotesCtrl'
   });
-}])
+}]);
 
-.controller('NotesCtrl', function NotesCtrl($scope, $http) {
+noteApp.controller('NotesCtrl', function NotesCtrl($scope, $http) {
   $http.get(elevennoteBasePath+'notes?api_key='+apiKey)
     .success(function(notes_data) {
       $scope.notes = notes_data;
     });
+});
+
+noteApp.controller('NoteFormCtrl', function NoteFormCtrl($scope){
+  $scope.body_html = 'This is the body';
 });
