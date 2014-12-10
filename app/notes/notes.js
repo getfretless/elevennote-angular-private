8176ae58349efd74571baac0a9d5e6666fcf831b
David Jones <unixmonkey1@gmail.com>
2014-12-10 12:14:31 -0500

    Show notes from API in sidebar

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var elevennoteBasePath = 'http://localhost:3000/api/v1/';
+var apiKey = '$2a$10$5GO4Gw5d9Snpy.KfhHDhJesJ4XKZ7iCTJaeUO0D0Z51T586TEbUTa';
+
 angular.module('myApp.notes', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +12,9 @@ angular.module('myApp.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesCtrl', [function() {
-
-}]);
+.controller('NotesCtrl', function NotesCtrl($scope, $http) {
+  $http.get(elevennoteBasePath+'notes?api_key='+apiKey)
+    .success(function(notes_data) {
+      $scope.notes = notes_data;
+    });
+});
