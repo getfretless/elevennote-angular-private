370c6a38a250b8750f878d02f1e4dce856212666
David Jones <unixmonkey1@gmail.com>
2014-12-11 01:11:16 -0500

    WIP: Creating notes

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -12,13 +12,34 @@ noteApp.config(['$routeProvider', function($routeProvider) {
   });
 }]);
 
-noteApp.controller('NotesCtrl', function NotesCtrl($scope, $http) {
+noteApp.controller('NotesCtrl', function NotesCtrl($scope, $http, $timeout) {
+  $scope.notes = [];
+  $scope.$watch('notes', function(){
+    console.log('notes updated');
+  });
+
   $http.get(elevennoteBasePath+'notes?api_key='+apiKey)
     .success(function(notes_data) {
       $scope.notes = notes_data;
     });
-});
 
-noteApp.controller('NoteFormCtrl', function NoteFormCtrl($scope){
   $scope.body_html = 'This is the body';
+  $scope.submit = function() {
+    console.log('submitting');
+    $http({
+      method: 'POST',
+      url: elevennoteBasePath + 'notes',
+      data: {
+        api_key: apiKey,
+        note: {
+          title: 'Title',
+          body_html: 'Body HTML'
+        }
+      }})
+      .success(function(note_data){
+        console.log(note_data);
+        console.log($scope.notes);
+        $scope.notes = $scope.notes.concat(note_data);
+      });
+  }
 });
