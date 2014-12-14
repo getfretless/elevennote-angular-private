e7096cc8629962fe47d32ab71478e41287e4644a
David Jones <unixmonkey1@gmail.com>
2014-12-14 10:39:31 -0500

    Move api-specific vars into service module

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var elevennoteBasePath = 'http://localhost:3000/api/v1/';
-var apiKey = '$2a$10$5GO4Gw5d9Snpy.KfhHDhJesJ4XKZ7iCTJaeUO0D0Z51T586TEbUTa';
-
 var noteApp = angular.module('myApp.notes', ['ngRoute']);
 
 noteApp.config(['$routeProvider', function($routeProvider) {
@@ -13,6 +10,8 @@ noteApp.config(['$routeProvider', function($routeProvider) {
 }]);
 
 noteApp.service('NotesBackend', function NotesBackend($http) {
+  var elevennoteBasePath = 'http://localhost:3000/api/v1/';
+  var apiKey = '$2a$10$5GO4Gw5d9Snpy.KfhHDhJesJ4XKZ7iCTJaeUO0D0Z51T586TEbUTa';
   var notes = [];
 
   this.getNotes = function() {
