73aa6ddd27266c35c3c09563d652a12177056b4f
David Jones <unixmonkey1@gmail.com>
2014-12-15 17:13:26 -0500

    Add login route/controller

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'myApp.notes'
+  'myApp.notes',
+  'myApp.login'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/notes'});
