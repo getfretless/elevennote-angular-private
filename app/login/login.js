a3b49e0e91ae1b8fbb0c8cae2bcfd2a0c572fe85
David Jones <unixmonkey1@gmail.com>
2014-12-17 22:48:06 -0500

    Add loginservice to get api key

--- app/login/login.js
+++ app/login/login.js
@@ -1,17 +1,35 @@
 'use strict';
 
-angular.module('myApp.login', ['ngRoute'])
+var loginApp = angular.module('myApp.login', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+loginApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {
     templateUrl: 'login/login.html',
     controller: 'LoginController'
   });
-}])
+}]);
 
-.controller('LoginController', function LoginController($scope, $location) {
+loginApp.service('SessionBackend', function SessionBackend($http) {
+  var elevennoteBasePath = 'http://localhost:3000/api/v1/';
+  var apiKey = '$2a$10$5GO4Gw5d9Snpy.KfhHDhJesJ4XKZ7iCTJaeUO0D0Z51T586TEbUTa';
+  var user;
+
+  this.fetchUser = function(user) {
+    $http.post(elevennoteBasePath+'session', {
+      user: {
+        username: user.username,
+        password: user.password
+      }
+    }).success(function(userData){
+      console.log('got user');
+      user = userData;
+    });
+  };
+});
+
+loginApp.controller('LoginController', function LoginController($scope, $location, SessionBackend) {
+  $scope.user = {};
   $scope.submit = function() {
-    // do something
-    console.log('dosomething')
+    SessionBackend.fetchUser($scope.user);
   };
 });
